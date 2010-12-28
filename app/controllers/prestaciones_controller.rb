7ba888938f728ba9a51f0a240dbcf2c47f35893d
Higinia <higinia@mathiasen-P4V8X-MX.(none)>
2010-12-28 20:07:16 -0300

    iteracion

--- app/controllers/prestaciones_controller.rb
+++ app/controllers/prestaciones_controller.rb
@@ -5,7 +5,7 @@ class PrestacionesController < ApplicationController
  before_filter :login_required
  layout 'default'
   def index
- @prestaciones = Prestacion.paginate :page=> params[:page], :per_page=> 2
+ @prestaciones = Prestacion.paginate :page=> params[:page], :per_page=> 5
    
     respond_to do |format|
       format.html # index.html.erb
@@ -107,7 +107,7 @@ class PrestacionesController < ApplicationController
     respond_to do |format|     
         @prestaciones = Prestacion.basic_search(params).paginate :page => params[:page], :per_page => 10
         
-        format.html {render :partial => 'resultado', :layout => 'prestaciones' }
+        format.html {render :partial => 'resultado', :layout => false }
        
       end
   end
