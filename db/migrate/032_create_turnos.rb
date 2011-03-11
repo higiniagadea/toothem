c087462d9393548be8209b3022d8393b8095a0c0
Higinia <higinia@mathiasen-P4V8X-MX.(none)>
2011-03-11 18:06:31 -0300

    tarea

--- db/migrate/032_create_turnos.rb
+++ db/migrate/032_create_turnos.rb
@@ -1,8 +1,8 @@
 class CreateTurnos <  ActiveRecord::Migration
   def self.up
     create_table "turnos" do |t|
-       t.column :fecha, :date, :null => false
-       t.column :hora, :time, :null => false
+       t.column :fecha_hora, :date, :null => false
+      
        t.column :duracion, :integer, :null=> false
        t.column :profesional_id, :integer, :null => false
        t.column :paciente_id, :integer, :null => false
