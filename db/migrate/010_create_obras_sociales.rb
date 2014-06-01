d25d4581680136ee551175555ee7a257dfb227ce
Higinia <higinia@gadea.(none)>
2014-06-01 00:46:38 +0000

    update obra social

--- db/migrate/010_create_obras_sociales.rb
+++ db/migrate/010_create_obras_sociales.rb
@@ -2,8 +2,8 @@ class CreateObrasSociales < ActiveRecord::Migration
   def self.up
     create_table "obras_sociales" do |t|
         t.column :nombre, :string, :limit => 30
-        t.column :domicilio, :string, :default => false
-        t.column :telefono, :string, :default => false
+        t.column :domicilio, :string,
+        t.column :telefono, :string, 
         t.column :codigos_por_mes, :integer
         t.column :correo_electronico; :string     
         t.column :deleted_at, :date
