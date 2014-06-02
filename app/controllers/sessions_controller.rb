bd81abad00203db1c60288ce2eb8d630f0420310
higiniagadea <higiniagadea@gmail.com>
2014-06-02 14:59:00 +0000

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@
 class SessionsController < ApplicationController
   # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
-  layout 'default'
+  #layout 'default'
  #layout 'application'
-   # layout 'login'
+    layout 'login'
   # render new.rhtml
   def new
     respond_to do |format|
