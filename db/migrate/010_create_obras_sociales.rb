e956d26c64da4a08f7bad2b2792d76204ab3afef
Higinia <higinia@gadea.(none)>
2014-06-02 09:47:08 +0000

    update models

--- db/migrate/010_create_obras_sociales.rb
+++ db/migrate/010_create_obras_sociales.rb
@@ -1,11 +1,11 @@
 class CreateObrasSociales < ActiveRecord::Migration
   def self.up
     create_table "obras_sociales" do |t|
-        t.column :nombre, :character, :limit => 30
-        t.column :domicilio, :character,
-        t.column :telefono, :character, 
+        t.column :nombre, :string, :limit => 30
+        t.column :domicilio, :string,:limit => 30
+        t.column :telefono, :string, :limit => 30
         t.column :codigos_por_mes, :integer
-        t.column :correo_electronico; :character     
+        t.column :correo_electronico; :string, :limit => 255, :null => false 
         t.column :deleted_at, :date, :null => true
 
     end
