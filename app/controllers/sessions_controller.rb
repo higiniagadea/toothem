d3e449ae4b63b7a5df7e41a1b9a0e86c2e30b511
Higinia <higinia@gadea.(none)>
2014-09-18 18:32:42 +0000

    update sesion

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
-# This controller handles the login/logout function of the site.  
 class SessionsController < ApplicationController
   # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
+  
  
-    layout 'default'
+    layout 'login'
+  # render new.rhtml
   def new
     respond_to do |format|
      unless current_usuario.blank? ? format.html : format.html {redirect_to search_pacientes_path}
@@ -18,41 +19,32 @@ class SessionsController < ApplicationController
    
       self.current_usuario = usuario
       new_cookie_flag = (params[:remember_me] == "1")
-
       session[:consultorio] = Consultorio.find(:first, :conditions => ['id in (?)', current_usuario.consultorios])
       session[:user_menu] = Item.find(:all, :conditions => 'parent_id = 1', :order => 'orden')
-
       paciente = Item.find(:first,:conditions => ["url = ?", '/pacientes'])
       params[:item_selected] = paciente
       generar_submenus
-
-      #session[:subitems] = Item.find(:all, :conditions => "lower(url) LIKE '%/profesionales/%'")
-
+      session[:subitems] = Item.find(:all, :conditions => "lower(url) LIKE '%/profesionales/%'")
       handle_remember_cookie! new_cookie_flag
       redirect_back_or_default('/pacientes/search')
       flash[:notice] = "Ingreso correcto"
-
     else
-     
       note_failed_signin
       @login = params[:login]
-      @remember_me = params[:remember_me]
-    
-      render :action => 'new'
-      
+      @remember_me = params[:remember_me]    
+      render :action => 'new'      
     end
   end
 
   def destroy
     logout_killing_session!
-    flash[:notice] = "Se ha desconectado."
+    flash[:notice] = "Se ha cerrado la sesion. Hasta Luego"
     redirect_back_or_default('/login')
   end
 
 protected
   # Track failed login attempts
-  def note_failed_signin
-    
+  def note_failed_signin    
     flash[:error] = "Los datos ingresados son incorrectos"
     logger.warn "Failed login for '#{params[:login]}' from #{request.remote_ip} at #{Time.now.utc}"
 
