e19df97559f4d46048ccce1ed55d44bbe9bb91c6
Higinia <higinia@gadea.(none)>
2014-09-29 16:06:44 +0000

    update pago pacientes

--- db/migrate/034_create_pagos_pacientes.rb
+++ db/migrate/034_create_pagos_pacientes.rb
@@ -6,7 +6,8 @@ class CreatePagosPacientes <  ActiveRecord::Migration
        t.column :nro_recibo, :string, :limit => 255
        t.column :fue_liquidado, :boolean
        t.column :paciente_id, :integer
-       t.column :fecha_liquidacion, :date
+       t.column :fecha_liquidacion,   
+       t.column :deleted_at, :date
 
       t.timestamps
     end
