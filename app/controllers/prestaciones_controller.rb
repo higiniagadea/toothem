3173348eddf22cbbf1fd38af484c17ec05fea23d
Higinia <higinia@gadea.(none)>
2014-10-02 13:04:46 +0000

    update controller prestaciones

--- app/controllers/prestaciones_controller.rb
+++ app/controllers/prestaciones_controller.rb
@@ -112,13 +112,9 @@ class PrestacionesController < ApplicationController
 
   def resultado  
     respond_to do |format|
-      if params[:codigo].blank? && params[:descripcion].blank?
-       format.html{render :text => '<span style="color:red">Ingrese al menos un dato para realizar la b&uacute;squeda </span>' }
-      elsif
         format.html{render :partial => 'resultado', :layout => false }
-         @prestaciones = Prestacion.basic_search(params).paginate(:page => params[:page], :per_page => 10, :order => 'descripcion')
-
-      end
+         @prestaciones = Prestacion.basic_search(params).paginate(:page => params[:page], :per_page => 10)
+  
     end
   end
 
